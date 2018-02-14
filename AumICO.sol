2689fdd2f353040218296a6007d6f6ec7bd15356
QuantumLex <lexgarza@gmail.com>
2018-02-14 00:33:31 -0600

    Create AumICO.sol

---
+++ AumICO.sol
@@ -0,0 +1,266 @@
+pragma solidity ^0.4.18;
+
+/**************************************************************
+*
+* Alteum ICO
+* Author: Lex Garza 
+* by ALTEUM / Copanga
+*
+**************************************************************/
+
+//import "github.com/oraclize/ethereum-api/usingOraclize.sol";
+import "./usingOraclize.sol";
+//import "github.com/oraclize/ethereum-api/oraclizeAPI.sol";
+
+interface token
+{
+	function sendCoin(address receiver, uint amount) public returns(bool sufficient);
+	function getBalance(address addr) public view returns(uint);
+}
+
+/*
+* Safe Math Library from Zeppelin Solidity
+* https://github.com/OpenZeppelin/zeppelin-solidity/blob/master/contracts/math/SafeMath.sol
+*/
+contract SafeMath
+{
+	function safeSub(uint256 a, uint256 b) internal pure returns (uint256) {
+		assert(b <= a);
+		return a - b;
+	}
+	
+	function safeDiv(uint256 a, uint256 b) internal pure returns (uint256) {
+		uint256 c = a / b;
+		return c;
+	}
+	
+	function safeMul(uint256 a, uint256 b) internal pure returns (uint256) {
+		if (a == 0) {
+			return 0;
+		}
+		uint256 c = a * b;
+		assert(c / a == b);
+		return c;
+	}
+}
+
+contract AumICO is usingOraclize, SafeMath {
+	//uint public tokenPricePreSale = 35; //Price x100 (with no cents: $0.35 => 35)
+	//uint public tokenPricePreICO = 55; //Price x100 (with no cents: $0.55 => 55)
+	//uint public tokenPriceICO = 75; //Price x100 (with no cents: $0.75 => 75)
+	//uint totalAvailableTokens = 31875000; // 37,500,000 AUM's available for sale, minus 5,625,000 sold in presale 
+	
+	struct OperationInQueue
+	{
+		uint operationStartTime;
+		uint depositedEther;
+		address receiver;
+		bool closed;
+	}
+	
+	uint[3] public tokenPrice;
+	uint[3] public availableTokens;
+	uint public tokenCurrentStage;
+	bool public hasICOFinished;
+	
+	uint public etherPrice; //Price x100 (with no cents: $800.55 => 80055)
+	uint public etherInContract;
+	
+	uint public startEpochTimestamp = 1518487231; // Testing
+	//uint public startEpochTimestamp = 1518807600; // Friday February 16th 2018 at 12pm GMT-06:00, you can verify the epoch at https://www.epochconverter.com/
+	uint public endEpochTimestamp = 1521093600; // Thursday March 15th 2018 at 12am GMT-06:00, you can verify the epoch at https://www.epochconverter.com/
+	
+	uint public lastPriceCheck = 0;
+	
+	uint preICOAvailableTokens = 11250000; // 11,250,000 AUM's for the pre ICO, with 8 decimals
+	uint ICOAvailableTokens = 20625000; // 20,625,000 AUM's for the pre ICO, with 8 decimals
+	
+	uint minAmmountToInvest = 100000000000000000; // 0.1 Ether, or 100,000,000,000,000,000 wei
+	uint maxAmmountToInvest = 500000000000000000000; // 500 Ether, or 500,000,000,000,000,000,000 wei
+	
+	address tokenContractAddress;
+	address admin;
+	mapping(address => bool) whitelist;
+	
+	token public tokenReward;
+	
+	OperationInQueue[] public operationsInQueue;
+	uint public currentOperation;
+	
+	modifier onlyAdmin()
+	{
+	    require(msg.sender == admin);
+	    _;
+	}
+	
+	event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
+	function AumICO() public {
+	    admin = msg.sender;
+		etherPrice = 80055; // testing
+		etherInContract = 0;
+		tokenPrice[0] = 35;//uint public tokenPricePreSale = 35; //Price x100 (with no cents: $0.35 => 35)
+		tokenPrice[1] = 55;//uint public tokenPricePreICO = 55; //Price x100 (with no cents: $0.55 => 55)
+		tokenPrice[2] = 75;//uint public tokenPriceICO = 75; //Price x100 (with no cents: $0.75 => 75)
+		availableTokens[0] = 0;
+		availableTokens[1] = preICOAvailableTokens * 10**8;
+		availableTokens[2] = ICOAvailableTokens * 10**8;
+		tokenCurrentStage = 0;
+		//tokenContractAddress = this;
+		tokenContractAddress = 0x8f0483125FCb9aaAEFA9209D8E9d7b9C8B9Fb90F;//Test, Token address on Ganache
+		tokenReward = token(tokenContractAddress);
+		currentOperation = 0;
+		hasICOFinished = false;
+		lastPriceCheck = now; // testing
+	}
+	
+	function changeTokenAddress (address newTokenAddress) public onlyAdmin
+	{
+		tokenContractAddress = newTokenAddress;
+		tokenReward = token(tokenContractAddress);
+	}
+	
+	function () payable {
+		if(!whitelist[msg.sender] || now < startEpochTimestamp || now >= endEpochTimestamp || hasICOFinished)
+		{
+			revert();
+		}
+        uint depositedEther = msg.value;
+        uint currentVaultBalance = tokenReward.getBalance(this);
+		if(depositedEther < minAmmountToInvest || depositedEther > maxAmmountToInvest)
+		{
+			revert();
+		}else if (currentVaultBalance > 0)
+		{
+			if(safeSub(now, lastPriceCheck) > 300)
+			{
+				operationsInQueue.push(OperationInQueue(now, depositedEther, msg.sender, false));
+				updatePrice();
+			}else
+			{
+				sendTokens(msg.sender, depositedEther);
+			}
+		}else 
+		{
+			revert();
+		}
+    }
+    
+	function sendTokens(address receiver, uint depositedEther) private 
+	{
+		if(tokenCurrentStage >= 3)
+		{
+			hasICOFinished = true;
+			receiver.transfer(depositedEther);
+		}else
+		{
+		    
+			uint obtainedTokensDividend = safeMul(etherPrice, depositedEther );
+			uint obtainedTokensDivisor = safeMul(tokenPrice[tokenCurrentStage], 10**10 );
+			uint obtainedTokens = safeDiv(obtainedTokensDividend, obtainedTokensDivisor);
+			
+			if(obtainedTokens > availableTokens[tokenCurrentStage])
+			{
+				if(availableTokens[tokenCurrentStage] > 0)
+				{
+					etherInContract += depositedEther;
+					tokenReward.sendCoin(receiver, availableTokens[tokenCurrentStage]);
+				}
+				
+				uint leftoverTokens = safeSub(obtainedTokens, availableTokens[tokenCurrentStage]);
+				availableTokens[tokenCurrentStage] = 0;
+				uint leftoverEtherDividend = safeMul(leftoverTokens, tokenPrice[tokenCurrentStage] );
+				leftoverEtherDividend = safeMul(leftoverEtherDividend, 10**10 );
+				uint leftoverEther = safeDiv(leftoverEtherDividend, etherPrice);
+				tokenCurrentStage++;
+				sendTokens(receiver, leftoverEther);
+			}else
+			{
+				availableTokens[tokenCurrentStage] = safeSub(availableTokens[tokenCurrentStage], obtainedTokens);
+				etherInContract += depositedEther;
+				tokenReward.sendCoin(receiver, obtainedTokens);
+			}
+		}
+	}
+	
+	function CheckQueue() private
+	{
+	    if(operationsInQueue.length > currentOperation)
+	    {
+    		if(!operationsInQueue[currentOperation].closed)
+    		{
+    		    operationsInQueue[currentOperation].closed = true;
+    			if(safeSub(now, operationsInQueue[currentOperation].operationStartTime) > 300)
+    			{
+    				operationsInQueue.push(OperationInQueue(now, operationsInQueue[currentOperation].depositedEther, operationsInQueue[currentOperation].receiver, false));
+    				updatePrice();
+    				currentOperation++;
+    				return;
+    			}else
+    			{
+    				sendTokens(operationsInQueue[currentOperation].receiver, operationsInQueue[currentOperation].depositedEther);
+    			}
+    		}
+    		currentOperation++;
+	    }
+	}
+	
+	function getTokenAddress() public constant returns (address) {
+		return tokenContractAddress;
+	}
+	
+	function getTokenBalance() public constant returns (uint) {
+		return tokenReward.getBalance(this);
+	}
+	
+	
+	function getEtherInContract() public constant returns (uint) {
+		return etherInContract;
+	}
+	
+	function GetQueueLength() public constant returns (uint) {
+		return safeSub(operationsInQueue.length, currentOperation);
+	}
+	
+	function AdvanceQueue() onlyAdmin public
+	{
+		CheckQueue();
+	}
+	
+	function AddToWhitelist(address addressToAdd) onlyAdmin public
+	{
+		whitelist[addressToAdd] = true;
+	}
+	
+	function RemoveFromWhitelist(address addressToRemove) onlyAdmin public
+	{
+		whitelist[addressToRemove] = false;
+	}
+	
+	function IsOnWhitelist(address addressToCheck) public view returns(bool isOnWhitelist)
+	{
+		return whitelist[addressToCheck];
+	}
+	
+	
+	function getPrice() public constant returns (uint) {
+		return etherPrice;
+	}
+	
+	function updatePrice() public 
+	{
+		if (oraclize_getPrice("URL") > this.balance) {
+            //LogNewOraclizeQuery("Oraclize query was NOT sent, please add some ETH to cover for the query fee");
+        } else {
+            //LogNewOraclizeQuery("Oraclize query was sent, standing by for the answer..");
+            oraclize_query("URL", "json(https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD).USD");
+        }
+	}
+	
+	function __callback(bytes32 _myid, string _result) {
+		require (msg.sender == oraclize_cbAddress());
+		etherPrice = parseInt(_result, 2);
+		lastPriceCheck = now;
+		CheckQueue();
+	}
+}
